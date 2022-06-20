3ce73252e2e5e8d0d63efacf2a5c3f82a833d21f
nineh90 <102158158+nineh90@users.noreply.github.com>
2022-06-21 01:43:29 +0200

    JavaScript implemented

---
+++ script.js
@@ -0,0 +1,67 @@
+let matchfield = [];
+let currentPlayer = 'circle';
+
+function fillMatchField(id){
+    matchfield[id] = currentPlayer;
+    
+    if(currentPlayer == 'circle'){
+        currentPlayer = 'cross';
+    }else{
+        currentPlayer = 'circle';
+    }
+    drawCurrentPlayer(); 
+    checkWinnerHorizontal();
+    checkWinnerVertical();
+    checkWinnerDiagonal();   
+    console.log(matchfield);
+}
+
+function drawCurrentPlayer(){
+    for (let i = 0; i < matchfield.length; i++) {
+        if (matchfield[i] == 'circle'){
+            document.getElementById('circle-' + i).classList.remove('d-none');
+        }
+        if (matchfield[i] == 'cross'){
+            document.getElementById('cross-' + i).classList.remove('d-none');
+        }
+    }
+}
+
+function checkWinnerHorizontal(){
+    let winner;
+    if(matchfield[0] == matchfield[1] && matchfield[1] == matchfield[2] && matchfield[0]){
+        winner = matchfield[0];
+    }  
+    if(matchfield[3] == matchfield[4] && matchfield[4] == matchfield[5] && matchfield[3]){
+        winner = matchfield[3];
+    }
+    if(matchfield[6] == matchfield[7] && matchfield[7] == matchfield[8] && matchfield[6]){
+        winner = matchfield[6];
+    }
+    console.log('Gewonnen');
+}    
+
+function checkWinnerVertical(){
+    let winner;
+    if(matchfield[0] == matchfield[3] && matchfield[3] == matchfield[6] && matchfield[0]){
+        winner = matchfield[0];
+    }
+    if(matchfield[1] == matchfield[4] && matchfield[4] == matchfield[7] && matchfield[1]){
+        winner = matchfield[1];
+    }
+    if(matchfield[2] == matchfield[5] && matchfield[5] == matchfield[8] && matchfield[2]){
+        winner = matchfield[2];
+    }
+    console.log('Gewonnen');
+}
+
+function checkWinnerDiagonal(){
+    let winner;
+    if(matchfield[0] == matchfield[4] && matchfield[4] == matchfield[8] && matchfield[0]){
+        winner = matchfield[0];
+    }
+    if(matchfield[2] == matchfield[4] && matchfield[4] == matchfield[6] && matchfield[2]){
+        winner = matchfield[2];
+    }
+    console.log('Gewonnen');
+}
