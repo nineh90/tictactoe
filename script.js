06d254b38c50cb8c60f443b679f8f992361c3d2e
nineh90 <102158158+nineh90@users.noreply.github.com>
2022-06-24 17:50:55 +0200

    modiefied all

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-let matchfield = [];
-let winner;
+let matchfield = []; 
+let winner; 
 let currentPlayer = 'circle';
 
-let crossSound = new Audio('./mp3/draw-cross.mp3');
 let circleSound = new Audio('./mp3/draw-circle.mp3');
 let gameOverSound = new Audio('./mp3/gameover.mp3');
+let noWinnerSound = new Audio('./mp3/no-winner.mp3');
 
-//textToSpeech();
 
 function fillMatchField(player){
     if (!matchfield[player]){
-        matchfield[player] = currentPlayer;
+        matchfield[player] = currentPlayer; 
         if(currentPlayer){
             if(currentPlayer == 'circle'){
              playerOne();
-            }else{
+            } else{
                playerTwo();
                 }
             }  
@@ -22,18 +21,17 @@ function fillMatchField(player){
         }        
 }
 
-function playerOne(){
+function playerOne(){ 
     currentPlayer = 'cross';
     document.getElementById('currentPlayerCircle').classList.add('inactive');
     document.getElementById('currentPlayerCross').classList.remove('inactive');
-    circleSound.play();
 }
 
 function playerTwo(){
     currentPlayer = 'circle';
     document.getElementById('currentPlayerCross').classList.add('inactive');
     document.getElementById('currentPlayerCircle').classList.remove('inactive');
-    crossSound.play();
+   
 }
 
 function checkForWinner(){ 
@@ -47,7 +45,7 @@ function checkForWinner(){
 function drawCurrentPlayer(){
     for (let i = 0; i < matchfield.length; i++) {
         if (circle(i)){
-            document.getElementById('circle-' + i).classList.remove('d-none');
+            document.getElementById('circle-' + i).classList.remove('d-none'); 
         }
         if (cross(i)){
             document.getElementById('cross-' + i).classList.remove('d-none');
@@ -56,57 +54,85 @@ function drawCurrentPlayer(){
 }
 
 function circle(i){
-    return matchfield[i] == 'circle'
+    return matchfield[i] == 'circle'; 
 }
 function cross(i){
-    return matchfield[i] == 'cross'
+    return matchfield[i] == 'cross';
 }
 
-function checkWinnerHorizontal(){
+function checkWinnerHorizontal(){      
     
-    if(matchfield[0] == matchfield[1] && matchfield[1] == matchfield[2] && matchfield[0]){
+    if(horizontalTop()){
         winner = matchfield[0];
         document.getElementById('line-1').style.transform = 'scalex(1)';
     }  
-    if(matchfield[3] == matchfield[4] && matchfield[4] == matchfield[5] && matchfield[3]){
+    if(horizontalMiddle()){
         winner = matchfield[3];
         document.getElementById('line-2').style.transform = 'scalex(1)';
     }
-    if(matchfield[6] == matchfield[7] && matchfield[7] == matchfield[8] && matchfield[6]){
+    if(horizontalBottom()){
         winner = matchfield[6];
         document.getElementById('line-3').style.transform = 'scalex(1)';
     }
-    if (winner) {
-       setTimeout(gameOver, 1000);
-    }
-}    
+    winnerExist();
+} 
+
+function horizontalBottom(){ 
+    return matchfield[6] == matchfield[7] && matchfield[7] == matchfield[8] && matchfield[6]
+}
+
+function horizontalMiddle(){
+    return matchfield[3] == matchfield[4] && matchfield[4] == matchfield[5] && matchfield[3]
+}
+
+function horizontalTop(){
+   return matchfield[0] == matchfield[1] && matchfield[1] == matchfield[2] && matchfield[0]
+}
 
 function checkWinnerVertical(){    
     if (winner) { return;} 
-    if(matchfield[0] == matchfield[3] && matchfield[3] == matchfield[6] && matchfield[0]){
+    if(verticalLeft()){
         winner = matchfield[0];
         document.getElementById('line-4').style.transform = 'rotate(90deg) scalex(1)';
     }
-    if(matchfield[1] == matchfield[4] && matchfield[4] == matchfield[7] && matchfield[1]){
+    if(verticalMiddle()){
         winner = matchfield[1];
         document.getElementById('line-5').style.transform = 'rotate(90deg) scalex(1)';
     }
-    if(matchfield[2] == matchfield[5] && matchfield[5] == matchfield[8] && matchfield[2]){
+    if(verticalBottom()){
         winner = matchfield[2];
         document.getElementById('line-6').style.transform = 'rotate(90deg) scalex(1)';
     }    
-    if (winner) {
-        setTimeout(gameOver, 1000);
-    }
+    winnerExist();
 }
+
+function verticalBottom(){
+    return matchfield[2] == matchfield[5] && matchfield[5] == matchfield[8] && matchfield[2]
+}
+
+function verticalMiddle(){
+    return matchfield[1] == matchfield[4] && matchfield[4] == matchfield[7] && matchfield[1]
+}
+
+function verticalLeft(){
+    return matchfield[0] == matchfield[3] && matchfield[3] == matchfield[6] && matchfield[0]
+}
+
+function winnerExist(i){
+    if (winner) {
+        setTimeout(gameOver, 500);
+     }
+}
+
+
 
 function checkWinnerDiagonal(){
     if (winner) { return;} 
-    if(matchfield[0] == matchfield[4] && matchfield[4] == matchfield[8] && matchfield[0]){
+    if(diagonalFromLeftUpToRightDown()){
         winner = matchfield[0];
         document.getElementById('line-7').style.transform = 'rotate(45deg) scalex(1)';
     }
-    if(matchfield[2] == matchfield[4] && matchfield[4] == matchfield[6] && matchfield[2]){
+    if(diagonalFromLeftDownToRightUp()){
         winner = matchfield[2];
         document.getElementById('line-8').style.transform = 'rotate(-45deg) scalex(1)';
     }
@@ -115,11 +141,24 @@ function checkWinnerDiagonal(){
     }
 }
 
+function diagonalFromLeftUpToRightDown(){
+     return matchfield[0] == matchfield[4] && matchfield[4] == matchfield[8] && matchfield[0]
+}
+
+function diagonalFromLeftDownToRightUp(){
+     return matchfield[2] == matchfield[4] && matchfield[4] == matchfield[6] && matchfield[2]
+}
+
 function noOneHasWon(){
-        if(matchfield[0] && matchfield[1] && matchfield[2] && matchfield[3] && matchfield[4] && matchfield[5] && matchfield[6] && matchfield[7]){ 
+        if(matchFieldisFull()){ 
             document.getElementById('gameWinner').innerHTML = 'Keiner hat Gewonnen';
-            gameOver(); 
+            gameOver();
+            noWinnerSound.play();
         } 
+}
+
+function matchFieldisFull(){
+   return matchfield[0] && matchfield[1] && matchfield[2] && matchfield[3] && matchfield[4] && matchfield[5] && matchfield[6] && matchfield[7]
 }
 
 function gameOver() {
@@ -127,28 +166,33 @@ function gameOver() {
     document.getElementById('endScreen').classList.remove('d-none');
     playerOneHasWon();
     playerTwoHasWon();
-    
-    //setTimeout(newStart, 5000);
 }
 
 function playerOneHasWon(){
-    if(winner == "circle"){
+    if(winnerIsCircle()){
     document.getElementById('gameWinner').innerHTML = 'Spieler 1 hat Gewonnen';
     gameOverSound.play();
     }
 }
+
+function winnerIsCircle(){
+    return winner == "circle";
+}
+
 function playerTwoHasWon(){
-    if(winner == "cross"){
+    if(winnerIsCross()){
     document.getElementById('gameWinner').innerHTML = 'Spieler 2 hat Gewonnen';
+    gameOverSound.play();
     }
 }
 
-
+function winnerIsCross(){
+    return winner == "cross";
+}
 
 function newStart(){
     window.location.reload();
 }
-
 
 function hideStartScreen() {
     document.getElementById('startScreen').classList.remove('d-flex');
